ea468cc3ffdb3820ea5724b51a6dc6e65582a2f9
MatValdejao <matvaldejao123@gmail.com>
2025-11-24 19:12:13 -0500

    created first problem I will solve

---
+++ problems/strings/zigzag_conversion.py
@@ -0,0 +1,11 @@
+"""
+LeetCode: ZigZag Conversion 
+
+Time: O(?)
+Space: O(?)
+
+"""
+
+class Solution:
+    def zigzagConvert(self, s: str, numRows: int) -> str:
+        return
---
+++ tests/zigzag_conversion_test.py
@@ -0,0 +1,8 @@
+""""
+Test File for ZigZag Conversion problem.
+"""
+
+from strings.zigzag_conversion import Solution
+
+def zigzag_conversion_test():
+    assert Solution().
