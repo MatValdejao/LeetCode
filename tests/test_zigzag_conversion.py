ca466daf78aca9a5b662f422b009732643c3cf07
MatValdejao <matvaldejao123@gmail.com>
2025-11-29 14:29:28 -0500

    added pytest file so python project added to sys path

--- tests/test_zigzag_conversion.py
+++ tests/test_zigzag_conversion.py
@@ -10,15 +10,15 @@ Normal Cases
 
 def test_example_1():
     # leetcode base case
-    assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
+    assert Solution().zigzagConvert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
 
 def test_example_2():
     # another leetcode base case
-    assert Solution().convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
+    assert Solution().zigzagConvert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
 
 def test_example_3():
     # one of my own
-    assert Solution().convert("ABCDEFGHIJKLMN", 4) == "AGMBFHLNCEIKDJ"
+    assert Solution().zigzagConvert("ABCDEFGHIJKLMN", 4) == "AGMBFHLNCEIKDJ"
 
 """
 Some edge cases (empty string, only two rows, rows to num of letter relationship)
@@ -26,17 +26,17 @@ Some edge cases (empty string, only two rows, rows to num of letter relationship
 
 def same_letter_count_rows():
     # same number of letters as rows
-    assert Solution().convert("Orange", 6) == "Orange"
+    assert Solution().zigzagConvert("Orange", 6) == "Orange"
 
 def one_row():
     # one row
-    assert Solution().convert("Orange", 1) == "Orange"
+    assert Solution().zigzagConvert("Orange", 1) == "Orange"
 
 def more_rows_than_letters():
     # more rows
-    assert Solution().convert("Orange", 8) == "Orange"
+    assert Solution().zigzagConvert("Orange", 8) == "Orange"
 
 def empty_string():
     # empty input string
-    assert Solution().convert("", 4) == ""
+    assert Solution().zigzagConvert("", 4) == ""
 
