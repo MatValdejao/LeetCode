e0d596099a7a2ff954338669f429ac5ec4d575ed
MatValdejao <matvaldejao123@gmail.com>
2025-11-29 14:03:50 -0500

    solution ready, will create inits so python can run/

--- problems/strings/zigzag_conversion.py
+++ problems/strings/zigzag_conversion.py
@@ -16,11 +16,22 @@ class Solution:
         idx, d = 0, 1
         rows = [[] for i in range(numRows)]
 
-        # string loop
+        # string loop and zigzag
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == (numRows - 1):
+                d = -1 
+            # update idx via direction
+            idx += d
+        if debug:
+            print(''.join([char for row in range(numRows) for char in rows[row]]))
 
-        return
+        return ''.join([char for row in range(numRows) for char in rows[row]])
 
 
+# main to be used for local debugging
 if __name__ == "__main__":
     # run inline with debug option
-    Solution().convert("PAYPALISHIRING", 3, debug=True) == "PAHNAPLSIIGYIR"
+    Solution().zigzagConvert("PAYPALISHIRING", 3, debug=True) == "PAHNAPLSIIGYIR"
