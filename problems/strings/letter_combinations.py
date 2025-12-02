f1f94ed0b000e6218ca4892dc38f1071a26e0913
MatValdejao <matvaldejao123@gmail.com>
2025-12-02 15:46:53 -0500

    define backtrack function I need

--- problems/strings/letter_combinations.py
+++ problems/strings/letter_combinations.py
@@ -18,4 +18,21 @@ class Solution:
         if any(d not in '23456789' for d in digits): 
             raise ValueError('Input must contain only digits between 2-9')
 
-        
+        # map digits
+        digits_map_phone = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        res = []
+
+        # create a bakctrack function for digits
+        def backtrack(idx, combination):
+            
+
