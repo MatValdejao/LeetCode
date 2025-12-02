04d74b00e98a6827eaf8817ee7f1e24452fdcfda
MatValdejao <matvaldejao123@gmail.com>
2025-12-02 15:34:15 -0500

    added 0 and 1 test

--- problems/strings/letter_combinations.py
+++ problems/strings/letter_combinations.py
@@ -11,4 +11,11 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # test for empty input
         if digits == '': return []
+
+        # test for 0 or 1, invalidate any string with 0 or 1
+        if any(d not in '23456789' for d in digits): 
+            raise ValueError('Input must contain only digits between 2-9')
+
+        
