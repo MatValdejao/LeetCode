e8e6b119c4bf596eb1000e48a7e68adb6a3bc310
MatValdejao <matvaldejao123@gmail.com>
2025-12-03 17:20:02 -0500

    added inline debug

--- problems/strings/longest_palindrome.py
+++ problems/strings/longest_palindrome.py
@@ -5,10 +5,8 @@ Space: O(?)
 
 """
 
-import re
-
 class Solution: 
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindrome(self, s: str, Debug=False) -> str:
         # test invalid inputs
         if len(s) == 0: raise ValueError('Input string must not be empty')
 
@@ -18,4 +16,6 @@ class Solution:
 
         # will attempt a two pointer solution from center
 
-       
+
+if __name__ == '__main__':
+    Solution().longestPalindrome('racecar', Debug=True) == 'racecar'
