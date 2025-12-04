80269af43c35b43682535a6057230c99599af153
MatValdejao <matvaldejao123@gmail.com>
2025-12-04 14:31:24 -0500

    added for loop to go through all palindromes, select biggest one, debugging

--- problems/strings/longest_palindrome.py
+++ problems/strings/longest_palindrome.py
@@ -13,13 +13,30 @@ class Solution:
         if not s.isalnum():
             raise ValueError('Input must contain only letters and digit')
 
+        # will attempt a two pointer solution from center
         def center_base(left, right):
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            
-        # will attempt a two pointer solution from center
+            return s[left:right]
+        
+        # initialize palindrome string as first letter of s
+        res = s[0]
 
+        # loop through input string, difference between odd count and even
+        for i in range(len(s) - 1):
+            # if odd start from same letter on center_base function
+            odd = center_base(i, i)
+            # offset start for even
+            even = center_base(i , i + 1)
+            
+            # now to check all palindromes found for largest
+            if len(odd) > len(res):
+                res = odd
+            if len(even) > len(res):
+                res = even
+
+        print(res)
 
 if __name__ == '__main__':
     Solution().longestPalindrome('racecar', Debug=True) == 'racecar'
