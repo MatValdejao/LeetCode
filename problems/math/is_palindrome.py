8d0e7c77ffc7b5d1d76605cb2cec3dad46151077
MatValdejao <matvaldejao123@gmail.com>
2025-12-09 13:23:37 -0500

    iterator works and builds correct reverse. Need to create placeholder for x I missed when testing

--- problems/math/is_palindrome.py
+++ problems/math/is_palindrome.py
@@ -11,9 +11,11 @@ import pytest
 
 class Solution: 
     def isPalindrome(self, x: int, Debug=False) -> bool:
+        # check no input 
         if x == None:
-           raise ValueError('Input must be a integer')
+           raise ValueError('Must input a number.')
 
+        # reverse with string conversion and compare
         reverse = str(x)[::-1]
         return reverse == str(x)
     
@@ -22,9 +24,28 @@ class SolutionNoConversion:
         # check for negative numbers
         if x < 0: return False
 
+        # check for no input
+        if x == None:
+            raise ValueError('Must input an number.')
+        
+        reverse = 0
+
+        # us remainder to get last integer
+        while x != 0:
+            # remainder of input, multiplying by ten to build inverse
+            reverse = reverse * 10 + x % 10
+            # print(reverse)
+            
+            # update x
+            x = x // 10
+            
+        return reverse == 
+            
+            
+
             
 
 # create debug call
 if __name__ == '__main__':
-    Solution().isPalindrome(12, Debug=True) == False
-    SolutionNoConversion().isPalindrome(12, Debug=True) = False
+    Solution().isPalindrome(12321, Debug=True) == False
+    SolutionNoConversion().isPalindrome(12321, Debug=True) == False
