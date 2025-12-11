c2471c5d5f93c0d2af993b11028d795d337665af
MatValdejao <matvaldejao123@gmail.com>
2025-12-11 15:38:49 -0500

    added inline debug

--- problems/array/median_sorted_array.py
+++ problems/array/median_sorted_array.py
@@ -12,3 +12,7 @@ from typing import List
 class Solution():
     def find_median_sorted_array(self, nums1: List[int], nums2: List[int]) -> float:
         
+
+
+if __name__ == '__main__':
+    Solution().find_median_sorted_array([1, 2], [3, 4]) == 2.5
