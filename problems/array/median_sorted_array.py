2bfc1265f367fbbbbff83854d31aaa85ea7c7dbb
MatValdejao <matvaldejao123@gmail.com>
2025-12-11 15:41:18 -0500

    combined lists and sorted

--- problems/array/median_sorted_array.py
+++ problems/array/median_sorted_array.py
@@ -7,12 +7,17 @@ Space: O(?)
 
 """
 
-from typing import List
+from typing import List 
 
 class Solution():
-    def find_median_sorted_array(self, nums1: List[int], nums2: List[int]) -> float:
-        
+    def find_median_sorted_array(self, nums1: List[int], nums2: List[int], debug=False) -> float:
+        # first step is tp combine and sort both lists
+        new_list = nums1 + nums2
+        new_list.sort()
+
+        if debug:
+            print(new_list)
 
 
 if __name__ == '__main__':
-    Solution().find_median_sorted_array([1, 2], [3, 4]) == 2.5
+    Solution().find_median_sorted_array([1, 2], [3, 4], debug=True) == 2.5
