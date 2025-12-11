7c56eb0cbb0b4b969efaa64542f7b5abafc6c6b6
MatValdejao <matvaldejao123@gmail.com>
2025-12-11 15:47:17 -0500

    added odd sized median checker

--- problems/array/median_sorted_array.py
+++ problems/array/median_sorted_array.py
@@ -14,10 +14,19 @@ class Solution():
         # first step is tp combine and sort both lists
         new_list = nums1 + nums2
         new_list.sort()
+        # initialize median
+        median = 0
 
-        if debug:
-            print(new_list)
+        # if debug:
+        #     print(new_list)
+        
+        # need to differentiate between odd sized and even for median
+        if len(new_list) % 2 == 1:
+            # for odd sized, find the middle index
+            median = new_list[len(new_list)//2]
+            if debug:
+                print(median)
 
 
 if __name__ == '__main__':
-    Solution().find_median_sorted_array([1, 2], [3, 4], debug=True) == 2.5
+    Solution().find_median_sorted_array([1, 2], [3, 4, 5], debug=True) == 2.5
