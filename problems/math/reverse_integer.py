eba84308d2ff9c0d7fbf6fb3562f266a852804fa
MatValdejao <matvaldejao123@gmail.com>
2025-12-15 14:33:12 -0500

    added main basic logic

--- problems/math/reverse_integer.py
+++ problems/math/reverse_integer.py
@@ -10,4 +10,27 @@ Space: O(?) ->
 import pytest
 
 class Solution:
-    def reverse(self, x: int) -> int:
+    def reverse(self, x: int, Debug=False) -> int:
+        if x >= 2**31 - 1:
+            raise ValueError('Outside bit-32 range')
+        
+        # check if number is neg and save
+        negative = False
+        if x < 0:
+            negative = True
+
+        # get absolute value
+        x = abs(x)
+
+        # convert to string, reverse and return reverse with checked neg
+        rev = str(x)[::-1]
+        if Debug:
+            print(rev)
+        
+        # check if number was negative and add back to return 
+        
+
+
+# inlined debug
+if __name__ == '__main__':
+    Solution().reverse(-123, Debug=True) == -321 
