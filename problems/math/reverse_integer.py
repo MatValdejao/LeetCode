547c0afaffa502e7f9e760e7b389af37e802df47
MatValdejao <matvaldejao123@gmail.com>
2025-12-15 14:03:47 -0500

    added base tests and no input test

---
+++ problems/math/reverse_integer.py
@@ -0,0 +1,12 @@
+"""
+
+LeetCode: Revere String
+
+Time: O(?) ->
+Space: O(?) ->
+
+"""
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
--- tests/test_reverse_integer.py
+++ tests/test_reverse_integer.py
@@ -6,6 +6,8 @@ Test file for reverse integer Leetcode problem
 
 from problems.math.reverse_integer import Solution
 
+# raise error for no input
+import pytest
 
 """
 
@@ -13,7 +15,17 @@ Normal Cases
 
 """
 
+# leetcode first base case
+def test_base_1():
+    assert Solution.reverse(123) == 321
 
+# second leetcode base case
+def test_base_2():
+    assert Solution.reverse(-123) == -321
+
+# third leetcode base case
+def test_base_3():
+    assert Solution.reverse(120) == 21
 
 
 """
@@ -21,3 +33,9 @@ Normal Cases
 Edge Cases
 
 """
+
+# test no input case
+def test_no_input():
+    with pytest.raises(ValueError):
+        Solution.reverse()
+
